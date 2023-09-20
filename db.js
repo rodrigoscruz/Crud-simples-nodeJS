b21232a366369c0ae3d2a14f74438e783388724c
Rodrigo <rs.strat@gmail.com>
2023-09-20 14:44:59 -0300

    Conectando no MySql

--- db.js
+++ db.js
@@ -3,8 +3,12 @@ async function connect(){
         return global.connection;
  
     const mysql = require("mysql2/promise");
-    const connection = await mysql.createConnection("mysql://root:luiztools@localhost:3306/crud");
+    const connection = await mysql.createConnection("mysql://root:@localhost:3307/crud");
     console.log("Conectou no MySQL!");
     global.connection = connection;
     return connection;
 }
+
+connect();
+
+module.exports = {}
